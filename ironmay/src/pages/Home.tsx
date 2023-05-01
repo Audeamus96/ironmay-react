a1fcff42696972cb5bf0f53e0d35e5b15b29fc92
Joshua Murphy <jjm19@sfu.ca>
2023-04-30 19:18:27 -0700

    Get users data

--- ironmay/src/pages/Home.tsx
+++ ironmay/src/pages/Home.tsx
@@ -4,10 +4,14 @@ import { Button, Form } from "react-bootstrap";
 
 import AuthContext from "../context/AuthProvider";
 import NavBar from "../components/NavBar";
+import { User } from "../models/user"
+import * as UserApi from '../network/users_api';
+
 
 const Home = () => {
     const navigate = useNavigate();
-    const { auth } = useContext(AuthContext)
+    const { auth } = useContext(AuthContext);
+    const [users, setUsers] = useState<User[]>([]);
 
     useEffect (() => {
         if (!auth){
@@ -16,6 +20,18 @@ const Home = () => {
         }
     }, [auth, navigate]);
 
+    useEffect (() => {
+        async function getUserData(){
+            try {
+                const users = await UserApi.getUsersData();
+                setUsers(users);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getUserData();
+    }, []);
+
     return (
         <>
             <NavBar
@@ -23,6 +39,9 @@ const Home = () => {
               onLogoutSuccessfull={ () => {navigate("/login")}}
             />
             <h1>Welcome {auth?.firstName}</h1>
+            {users.map((user) => (
+                <p>{user.firstName}</p>
+            ))}
         </>
       );
 }

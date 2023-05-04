ae67a826713038dc07046d36e14a6a66908e19f0
Joshua Murphy <jjm19@sfu.ca>
2023-05-03 23:56:22 -0700

    Added new routes for acctivity summaries

--- backend/src/routes/teams.ts
+++ backend/src/routes/teams.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import { requiresAuth } from "../middleware/auth";
+
 import * as TeamsController from "../controllers/teams";
+import { getTeamActivitySummary } from "../controllers/activities";
 
 const router = express.Router();
 
 router.get("/", TeamsController.getTeams);
+
+router.get("/summary", requiresAuth ,getTeamActivitySummary);
 
 router.get("/:teamId", TeamsController.getTeam);
 
@@ -11,6 +16,6 @@ router.post("/", TeamsController.createTeam);
 
 router.patch("/:teamId", TeamsController.updateTeam);
 
-router.delete("/:teamId", TeamsController.deleteTeam);
+router.delete("/:teamId", requiresAuth, TeamsController.deleteTeam);
 
 export default router;
